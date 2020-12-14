932496d0163fcbfe4a4564742e9943e5ace73234
unknown <Ganesh553>
2020-12-14 08:46:43 +0530

    AddressBook

---
+++ AddressBookMain.java
@@ -0,0 +1,84 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		AddressBook ab=new AddressBook();
+		int choice=0;
+		boolean flag=true;
+		
+		while(flag==true) {
+			System.out.println("1. Adding new Person");
+			System.out.println("2. Editing Person");
+			System.out.println("3. deleting Person");
+			System.out.println("4. Display all Contact details");
+			System.out.println("5. Check duplicate element");
+			System.out.println("6. Search by using State");
+			System.out.println("7. Search by using City");
+			System.out.println("8. Search Person");			
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1: System.out.println("Adding New User");
+					System.out.println("Enter First Name");
+					String firstName=sc.next();
+					System.out.println("Enter Last Name");
+					String lastName=sc.next();
+					System.out.println("Enter Address");
+					String address=sc.next();
+					System.out.println("Enter city");
+					String city=sc.next();
+					System.out.println("Enter state");
+					String state=sc.next();
+					System.out.println("Enter Zip");
+					int zip=sc.nextInt();
+					System.out.println("Enter phone number");
+					long phoneNumber=sc.nextLong();
+					Person person = new Person(firstName,lastName,address,city,state,zip,phoneNumber);
+					ab.addingNewPerson(person);
+					break;
+			case 2: System.out.println("Editing Person");
+			        System.out.println("Enter the firstName of the Person that you wish to edit");
+			        String editName=sc.next();
+			        ab.editingPerson(editName);
+			        break;
+			case 3: System.out.println("Deleting Person");
+					System.out.println("Enter the firstName of the person that you wish to delete");
+					String deleteName=sc.next();
+					ab.deletingPerson(deleteName);
+					break;
+			case 4: System.out.println("Displaying all the contacts ");
+					ab.display();
+					break;
+			case 5: System.out.println("Enter the First name to check duplicate");
+					ab.duplicatEntry();
+					break;
+			case 6: System.out.println("Enter the State to see the person");
+					String searchState=sc.next();
+					ab.searchState(searchState);
+					break;
+			case 7: System.out.println("Enter the City to see the person");
+					String searchCity=sc.next();
+					ab.searchCity(searchCity);
+					break;
+			case 8: System.out.println("Enter the Person");
+					String searchName = sc.next();
+					ab.searchPerson(searchName);
+			
+			}
+			System.out.println("Do you wish to continue y/n");
+			char ans = sc.next().charAt(0);
+			 if(ans=='y') {
+				 flag=true;
+			 }
+			 else {
+				 System.out.println("Thank you");
+				 flag=false;
+			 }
+		}
+	}
+
+}
